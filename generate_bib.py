bf2720e647ffd4d33861a781a123f7e059008142
Pascal Welke <welke@uni-bonn.de>
2023-05-20 21:51:10 +0200

    further work on reference formatter

--- generate_bib.py
+++ generate_bib.py
@@ -4,9 +4,11 @@ from bibtexparser.bparser import BibTexParser
 
 
 def filter_pubtype(list, pubtype):
-    return [b for b in list if b['publicationtype'] == pubtype]
+    '''filter by pubtype and sort in reverse chronological order'''
+    filtered_list = [b for b in list if b['publicationtype'] == pubtype]
+    return sorted(filtered_list, key=lambda b: (b['year'], b['author']), reverse=True)
 
-def listitem(item):
+def format_bibitem(item):
     if 'doi' in item:
         link = f'https://dx.doi.org/{item["doi"]}'
     elif 'url' in item:
@@ -14,9 +16,30 @@ def listitem(item):
     else:
         link = ''
 
-    s =  f'<li>{item["author"]}:<br />\n<a href="{link}">{item["title"]}</a><br />\n'
-    if 'journal' in item:
-        pass
+    # authors and title
+    s =  f'<li>{item["author"]} ({item["year"]}):<br />\n'
+    s += f'<a href="{link}">{item["title"]}</a>.<br />\n'
+
+    # venue formatting
+    if item['ENTRYTYPE'] == 'article':
+        s += f'{item["journal"]}, {item["publisher"]}<br />'
+    if item['ENTRYTYPE'] == 'inproceedings':
+        s += f'{item["booktitle"]}, {item["publisher"]}<br />'
+
+    # footer row with optionals
+    if 'url' in item:
+        s += f'[<a href="{item["url"]}">pdf</a>]\n'
+    if 'poster' in item:
+        s += f'[<a href="{item["poster"]}">poster</a>]\n'
+    if 'video' in item:
+        s += f'[<a href="{item["video"]}">poster</a>]\n'
+    if 'codeurl' in item:
+        s += f'[<a href="{item["codeurl"]}">code</a>]\n'
+    if 'doi' in item:
+        s += f'[<a href="https://dx.doi.org/{item["doi"]}">doi</a>]\n'
+    if 'biburl' in item:
+        s += f'[<a href="{item["biburl"]}">bibtex</a>]\n'
+    
     s += '</li>\n'
     return s
 
@@ -24,7 +47,7 @@ def listitem(item):
 def get_pubtype_html(list, pubtype):
     s = '<ol>\n'
     for b in filter_pubtype(list, pubtype):
-        s += listitem(b)
+        s += format_bibitem(b)
     s += '</ol>\n'
     return s
 
